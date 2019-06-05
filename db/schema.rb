adb4690edcd4d78bb63e3dee0a10bc149934c0b2
Brandon Johnson <BrandonJohnson659@gmail.com>
2019-06-05 21:43:38 +0000

    scaffold tweeet

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_212122) do
+ActiveRecord::Schema.define(version: 2019_06_05_214144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweeets", force: :cascade do |t|
+    t.text "tweeet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
